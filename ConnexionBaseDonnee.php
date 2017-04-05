7a6265bff6c43c3338cf128cd45fda5a4022885e
alex03ben <alexandre.benkheira@gmail.com>
2017-04-05 08:55:09 +0200

    affichage données test

--- ConnexionBaseDonnee.php
+++ ConnexionBaseDonnee.php
@@ -2,4 +2,7 @@
 <?php
 $connect = mysql_connect('localhost','root','') or die ("erreur de connexion");
 mysql_select_db('gestionsiteimage',$connect) or die ("erreur de connexion base");
+
+
+echo $connect->exec('SELECT * FROM image');
 ?>
