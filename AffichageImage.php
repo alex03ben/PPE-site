3a7c6353d0d8910af403a362f95fb00bf78018de
alex03ben <alexandre.benkheira@gmail.com>
2017-04-05 11:07:03 +0200

    grgr

--- AffichageImage.php
+++ AffichageImage.php
@@ -1,5 +1,5 @@
 <?php
-for(int i = 0; i < 15; i++)
+for(int $i = 0; $i < 15; $i++)
 {
 ?>
 <!DOCTYPE html>
@@ -8,6 +8,7 @@ for(int i = 0; i < 15; i++)
 	<link rel="stylesheet" href="CSS.css" />
 	<title>Test</title>
 	<center><h1 class="couleurTexte">Choisissez une image : </h1></center>
+	<center><h2> <<?php echo $i; ?></h2>
 </head>
 <body>
 	<p class="configPseudo">Pseudo : <?php echo $_POST['pseudo']; ?></p>
