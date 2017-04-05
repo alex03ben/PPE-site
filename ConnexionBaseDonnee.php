921cad379a0ed1a72caed123bfe7e454f4ebbe1d
alex03ben <alexandre.benkheira@gmail.com>
2017-04-05 10:30:42 +0200

    pd

--- ConnexionBaseDonnee.php
+++ ConnexionBaseDonnee.php
@@ -1,6 +1,6 @@
 
 <?php
-try
+/*try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=gestionsiteimage;charset=utf8', 'root', 'root');
 }
@@ -15,7 +15,27 @@ if($bdd->exec('SELECT * FROM utilisateurs')){
 	echo $req;
 }else{
 	echo "ça marche pas";
-}
+}*/
+
+try {
+    $hostname = "servername";
+    $dbname = "dbname";
+    $username = "username";
+    $pw = "password";
+    $pdo = new PDO ("mssql:host=$hostname;dbname=$dbname","$username","$pw");
+  } catch (PDOException $e) {
+    echo "Failed to get DB handle: " . $e->getMessage() . "\n";
+    exit;
+  }
+      $query = $pdo->prepare("select name FROM tbl_name");
+      $query->execute();
+     
+      for($i=0; $row = $query->fetch(); $i++){
+        echo $i." - ".$row['name']."<br/>";
+      }
+
+      unset($pdo);
+      unset($query);
 
 
 ?>
