3106676897486548029c3b1b9e22dd921be0e7e0
alex03ben <alexandre.benkheira@gmail.com>
2017-05-17 11:16:36 +0200

    ff

--- ConnexionBaseDonnee.php
+++ ConnexionBaseDonnee.php
@@ -9,7 +9,7 @@ catch(Exception $e)
 {
     die('Erreur : '.$e->getMessage());
 }
-$requete = "SELECT * FROM image WHERE id=\""+$nb1+"\"";
+$requete = "SELECT * FROM image WHERE id=\"5\"";
 
 $req = $bdd->query($requete);
 $affiche = $req->fetch();
