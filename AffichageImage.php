c9b23d6ec7f02587c220252f4326a40ab65af92c
alex03ben <alexandre.benkheira@gmail.com>
2017-05-17 09:00:47 +0200

    fgvbd
    gb

--- AffichageImage.php
+++ AffichageImage.php
@@ -1,11 +1,10 @@
 <?php
 
-$nb1 = rand(1,6);
-$nb2 = rand(1,10);
-$nb3 = rand(1,10);
+$nb1 = rand(1,4);
+$nb2 = rand(1,5);
+$nb3 = rand(1,4);
 $nb4 = rand(1,10);
 
-//echo "<img src=\"image/img"+$nb2+".jpg\" class=\"animImage\">";
 ?>
 
 <!DOCTYPE html>
@@ -20,13 +19,11 @@ $nb4 = rand(1,10);
 	<p class="configPseudo">Pseudo : <?php echo $_POST['pseudo']; ?></p>
 	<p class="imageCentre">
 		<center>
-			<a href="AffichageImage.php"><?php echo "<img src=\"image/img",$nb1,".jpg\" class=\"animImage\">"; ?></a>
-			<a href="AffichageImage.php"><?php echo "<img src=\"image/img",$nb2,".jpg\" class=\"animImage\">"; ?></a>
-			<a href="AffichageImage.php"><?php echo "<img src=\"image/img",$nb3,".jpg\" class=\"animImage\">"; ?></a>
-			<a href="AffichageImage.php"><?php echo "<img src=\"image/img",$nb4,".jpg\" class=\"animImage\">"; ?></a>		
-			
+			<a href="AffichageImage.php"><?php echo "<img src=\"image/gentil/img",$nb1,".jpg\" class=\"animImage\">"; ?></a>
+			<a href="AffichageImage.php"><?php echo "<img src=\"image/keke/img",$nb2,".jpg\" class=\"animImage\">"; ?></a>
+			<a href="AffichageImage.php"><?php echo "<img src=\"image/psychopathe/img",$nb3,".jpg\" class=\"animImage\">"; ?></a>
+			<a href="AffichageImage.php"><?php echo "<img src=\"image/img",$nb4,".jpg\" class=\"animImage\">"; ?></a>				
 		</center>
 	</p>
 </body>
 </html>
-
