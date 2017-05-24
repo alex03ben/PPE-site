0786c079d4b5739d41876d9a2f373e5f9c2c3031
alex03ben <alexandre.benkheira@gmail.com>
2017-05-24 08:21:02 +0200

    fzfz
    fzz

--- AffichageImage.php
+++ AffichageImage.php
@@ -10,7 +10,7 @@ catch(Exception $e)
     die('Erreur : '.$e->getMessage());
 }
 
-$req = $bdd->query("SELECT chemin FROM image WHERE id = '$nb1' AND chemin like = '%psychopathe%' ");
+$req = $bdd->query("SELECT chemin FROM image WHERE chemin like = \"%psychopathe%\" AND id = '$nb1' ");
 
 $affiche = $req->fetch()
 
